005e3858993b0e67e8d7d3e1e8feae468443366e
abhishekksingh02 <141712253+abhishekksingh02@users.noreply.github.com>
2023-12-29 12:19:20 +0530

    Add files via upload

---
+++ JAVA CORE BASICS/MODULE-2/Module2Ex11.java
@@ -0,0 +1,9 @@
+package core_Java;
+
+import java.util.Calendar;
+
+public class Module2Ex11 {
+  public static void main(String[] args) {
+	System.out.println(Calendar.getInstance().getTime());
+}	
+}
