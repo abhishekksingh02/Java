bb00662ede8804e94dbfed4c331beba329df89fa
abhishekksingh02 <141712253+abhishekksingh02@users.noreply.github.com>
2024-03-14 10:15:51 +0530

    Add files via upload

---
+++ JAVA CORE BASICS/PRACTICE/NumberPattern1.java
@@ -0,0 +1,18 @@
+package JavaPracs;
+
+public class NumberPattern1 {
+   public static void main(String[] args) {
+	
+	   int i,j;
+	   
+	   for(i=1;i<=5;i++)
+	   {
+		   for(j=1;j<=i;j++)
+		   {
+			   System.out.print(j);
+		   }
+		   System.out.println();
+	   }
+//	   System.out.println();
+}
+}
